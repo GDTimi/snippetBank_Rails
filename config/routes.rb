c1255409d0bfc790cecd2d32d28467b317894731
GDTimi <atimiw@gmail.com>
2024-01-17 16:32:59 +0000

    Entries controller and initial route established, pre-setting up the models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "entries#index"
+
+  get "/", to: "entries#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
