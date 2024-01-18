31d2fcb5e3c09b7f8f821bc9890d34a300dd0f4f
GDTimi <atimiw@gmail.com>
2024-01-18 11:03:36 +0000

    Initial language seeding set up, enough to work with for building out the project (user additional languages could be built out later).

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Seeding Languages:"
+
+languages = [
+  { name: "Unset"},
+  { name: "C"},
+  { name: "Clojure"},
+  { name: "CSS"},
+  { name: "C++"},
+  { name: "C#"},
+  { name: "HTML5"},
+  { name: "Java"},
+  { name: "JavaScript"},
+  { name: "Pascal"},
+  { name: "PHP"},
+  { name: "Python"},
+  { name: "Ruby"},
+  { name: "SQL"},
+  { name: "TypeScript"},
+]
+
+languages.each do |language|
+  Language.find_or_create_by(language)
+  puts "Created language: #{language[:name]}"
+end
+
+puts "Language seeding complete."
